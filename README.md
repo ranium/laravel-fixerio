fc8396fb40be20d5911510a1c7e12f677a0dfc98
Abbas Ali <abbas@ranium.in>
2019-02-05 19:01:33 +0530

    Added feature to cache the results. First stable release complete.

--- README.md
+++ README.md
@@ -19,7 +19,8 @@ Edit the `config/fixerio.php` and put your access key and tweak other config opt
 ## Usage
 
 ```php
-$fixerio = app(\Ranium\Fixerio\Client::class);
+// Resolve the client class instance out of the service container
+$fixerio = app(\Ranium\LaravelFixerio\Client::class);
 
 // Find latest rates
 $latestRates = $fixerio->latest();
@@ -31,6 +32,39 @@ $historicalRates = $fixerio->historical(['date' => '2019-01-01']);
 
 echo $historicalRates['rates']['INR'];
 ```
+
+You can use the provided Facade as well
+```php
+use Fixerio;
+
+$latestRates = Fixerio::latest();
+```
+As there is a hard limit on number of requests you can make to fixer.io, this package provides an easy way to cache the responses. Default cache storage is used for caching.
+
+Modify the `config/fixerio.php` to enable the caching.
+
+```php
+'cache' => [
+    'enabled' => true,
+    'expire_after' => 60, // In minutes, change this as per requirement
+];
+```
+
+You can disable cache in the runtime.
+
+```php
+use Fixerio;
+
+Fixerio::disableCache();
+
+$latestRates = Fixerio::latest();
+
+// Enable cache again for other calls
+Fixerio::enableCache();
+
+// Other calls to the API...
+```
+
 Please refer [ranium/fixerio-php-client](https://github.com/ranium/fixerio-php-client) for all available API calls and other details.
 
 The response for all the above calls will be a Guzzle Command Result object. Please refer fixer.io's [documentation](https://fixer.io/documentation) for further details about various endpoints, request parameters and response objects.
---
+++ src/Client.php
@@ -0,0 +1,90 @@
+<?php
+namespace Ranium\LaravelFixerio;
+
+use Ranium\Fixerio\Client as BaseClient;
+
+/**
+ * Client class for Fixerio
+ *
+ * @author Abbas Ali <abbas@ranium.in>
+ */
+class Client extends BaseClient
+{
+    /**
+     * Whether to cache the results or not
+     *
+     * @var boolean
+     */
+    protected $cacheResult = false;
+
+    /**
+     * Creates and executes a command for an operation by name.
+     *
+     * We are overriding \GuzzleHttp\Command\ServiceClient::__call()
+     * so that we can cache the responses.
+     *
+     * @param string $name Name of the command to execute.
+     * @param array  $args Arguments to pass to the getCommand method.
+     *
+     * @return GuzzleHttp\Promise\ResultInterface|GuzzleHttp\Promise\PromiseInterface
+     * @see \GuzzleHttp\Command\ServiceClientInterface::getCommand
+     */
+    public function __call($name, $args)
+    {
+        // If cache is enabled, then get the result from cache
+        if ($this->cacheResult) {
+            $key = $this->getCacheKey($name, $args);
+
+            return cache()->remember(
+                $key,
+                config('fixerio.cache.expire_after'),
+                function () use ($name, $args) {
+                    return parent::__call($name, $args);
+                }
+            );
+        }
+
+        // No caching, call the parent's __call() method
+        return parent::__call($name, $args);
+    }
+
+    /**
+     * Method to construct the cache key.
+     *
+     * Any change in the arguments or fixerio config
+     * will bust the cache for the given method.
+     *
+     * @param string $name Name of the method being called
+     * @param array  $args Arguments being passed to the method
+     *
+     * @return string Cache key
+     */
+    private function getCacheKey($name, $args)
+    {
+        // Cache key will be and hash of args and config (prefixed with method name)
+        return $name . '_' . md5(
+            json_encode($args) .
+            json_encode(config('fixerio'))
+        );
+    }
+
+    /**
+     * Method to disable the caching of results
+     *
+     * @return void
+     */
+    public function disableCache()
+    {
+        $this->cacheResult = false;
+    }
+
+    /**
+     * Method to enable the caching of results
+     *
+     * @return void
+     */
+    public function enableCache()
+    {
+        $this->cacheResult = true;
+    }
+}
